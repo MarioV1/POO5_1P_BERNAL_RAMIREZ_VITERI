68adf481de9efedbb512d0ef583309b763478952
Kevin Ramirez <kevgiram@espol.edu.ec>
2024-06-19 01:36:59 -0500

    Añadi las variables de instancia juntos a sus setters y getters, y añadi el constructor de la subclase Editor.

--- GestorRevista/Editor.java
+++ GestorRevista/Editor.java
@@ -1,5 +1,41 @@
 package GestorRevista;
 
 public class Editor extends Usuario {
+    private String journal;
+    private String usuario;
+    private String contraseña;
     
+    //constructor
+    public Editor(String journal, String usuario, String contraseña){
+        this.journal = journal;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+    //getters y setters
+    public String getJournal(){
+        return journal;
+    }
+
+    public void setJornal(String journal){
+        this.journal = journal;
+    }
+
+    public String getUsuario(){
+        return usuario;
+    }
+
+    public void setUsuario(String usuario){
+        this.usuario = usuario;
+    }
+
+    public String getContraseña(){
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña){
+        this.contraseña = contraseña;
+    }
+
+
 }
