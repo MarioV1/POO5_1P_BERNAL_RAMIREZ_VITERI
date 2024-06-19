922e8258a7a32ae73d74c60f99cf20e89e0dfd37
MarioV1 <mar.vit99@hotmail.com>
2024-06-19 13:55:11 -0500

    Cambios en la clase Usuario Se agregaron constructores

--- GestorRevista/Usuario.java
+++ GestorRevista/Usuario.java
@@ -5,6 +5,22 @@ public class Usuario {
     protected String nombre;
     protected String apellido;
     protected String correo;
+    //Constructor 
+    public Usuario(){
+        this.nombre="";
+        this.apellido="";
+        this.correo="";
+    }
+    public Usuario(String nombre, String apellido, String correo){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.correo=correo;
+    }
+    //Métodos 
+    public void agregarUsuario(Usuario usuario){
+
+
+    }
 
     
 }
