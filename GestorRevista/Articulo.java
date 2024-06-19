9aa37a5fcaa6ab721bcc40a0e4937b4d9076cfa3
Mateo Bernal <msbernal@espol.edu.ec>
2024-06-18 23:40:28 -0500

    Añadi las variables de instancia y los constructores

--- GestorRevista/Articulo.java
+++ GestorRevista/Articulo.java
@@ -1,5 +1,21 @@
 package GestorRevista;
 
 public class Articulo {
-    
+    public String codigo;
+    public Autor datosAutor;
+    public String resumen;
+    public String contenido;
+    public String palabrasClave;
+    public String comentarios;
+    public EstadoAriculo estadoAriculo;
+    public Articulo(String codigo, Autor datosAutor,String resumen,String contenido,String palabrasClave,String comentarios,EstadoAriculo estadoAriculo){
+        this.codigo=codigo;
+        this.datosAutor=datosAutor;
+        this.resumen=resumen;
+        this.contenido=contenido;
+        this.comentarios=comentarios;
+        this.estadoAriculo=estadoAriculo;
+
+    }
+
 }
--- GestorRevista/Autor.java
+++ GestorRevista/Autor.java
@@ -1,5 +1,12 @@
 package GestorRevista;
 
 public class Autor extends Usuario {
-    
+    private String codigoID;
+    private String institucion;
+    private String campo;
+    public Autor(String codigoID, String institucion,String campo){
+        this.codigoID=codigoID;
+        this.institucion=institucion;
+        this.campo=campo;
+    }
 }
