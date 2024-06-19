f1abc20a3bb7618e5d71a451100f4309a2692342
Kevin Ramirez <kevgiram@espol.edu.ec>
2024-06-19 02:01:16 -0500

    Añadi las variables instancia y el constructor de la clase GestionarArticulo

--- GestorRevista/GestionarArticulo.java
+++ GestorRevista/GestionarArticulo.java
@@ -1,5 +1,24 @@
 package GestorRevista;
 
+import java.util.ArrayList;
+
 public class GestionarArticulo {
-    
+    //Variables de instancia
+    public Articulo articulo;
+    public ArrayList<Revisor> revisor;
+    public Editor editor;
+    public int id;
+    public String fecha; 
+
+    //constructor
+
+    public GestionarArticulo(Articulo articulo, ArrayList<Revisor> revisor, Editor editor, int id, String fecha){
+        this.articulo = articulo;
+        this.revisor = revisor;
+        this.editor = editor;
+        this.id = id;
+        this.fecha = fecha;
+    }
+
+
 }
