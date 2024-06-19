b1fcfd00937af08a726f60d4e565a2bbfd1df539
Kevin Ramirez <kevgiram@espol.edu.ec>
2024-06-19 01:54:31 -0500

    agregue las variables de instancia, constructores, setters y getters de la subclase Revisor

--- GestorRevista/Revisor.java
+++ GestorRevista/Revisor.java
@@ -1,5 +1,54 @@
 package GestorRevista;
 
 public class Revisor extends Usuario{
+    //Variables de instancia
+    private String especialidad;
+    private String usuario;
+    private String contraseña;
+    private int numArticulos;
+
+    //Constructor
+    public Revisor(String especialidad, String usuario, String contraseña, int numArticulos){
+        this.especialidad = especialidad;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.numArticulos = numArticulos;
+    }
+
+
+    //getters y setters
     
+    public String getEspecialidad(){
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad){
+        this.especialidad = especialidad;
+    }
+
+    public String getUsuario(){
+        return usuario;
+    }
+
+    public void setUsuario(String usuario){
+        this.usuario = usuario;
+    }
+
+    public String getContraseña(){
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña){
+        this.contraseña = contraseña;
+    }
+
+    public int getNumArticulos(){
+        return numArticulos;
+    }
+
+    public void setNumArticulos(int numArticulos){
+        this.numArticulos = numArticulos;
+    }
+
+
 }
