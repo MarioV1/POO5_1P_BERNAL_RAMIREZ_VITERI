5527d55ed9da8f6322649646009cf6286c854601
MarioV1 <mar.vit99@hotmail.com>
2024-06-27 14:38:13 -0500

    Cambios leves en la clase autor Se agregaron getters y setters faltantes en la clase Autor, también se verifica a través de esta actualización el correcto funcionamiento del repositorio

--- GestorRevista/Autor.java
+++ GestorRevista/Autor.java
@@ -1,12 +1,33 @@
 package GestorRevista;
 
 public class Autor extends Usuario {
+    //Atributos
     private String codigoID;
     private String institucion;
     private String campo;
+    //Constructor
     public Autor(String codigoID, String institucion,String campo){
         this.codigoID=codigoID;
         this.institucion=institucion;
+        this.campo=campo;
+    }
+    //Getters y Setters
+    public String getCodigoID(){
+        return this.codigoID;
+    }
+    public void setCodigoID(String codigoID){
+        this.codigoID=codigoID; 
+    }
+    public String getInstitucion(){
+        return this.institucion;
+    }
+    public void setInstitucion(String institucion){
+        this.institucion=institucion;
+    }
+    public String getCampo(){
+        return this.campo;
+    }
+    public void setCampo(String campo){
         this.campo=campo;
     }
 }
