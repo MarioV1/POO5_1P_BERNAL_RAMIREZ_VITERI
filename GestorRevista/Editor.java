13b4c31c85288f2098459fd6c91749dc20891555
Kevgiram <kevgiram@espol.edu.ec>
2024-07-07 00:56:40 -0500

    Agregue en el archivo .txt, los datos necesarios para la clase editor

--- GestorRevista/Editor.java
+++ GestorRevista/Editor.java
@@ -1,5 +1,14 @@
 package gestorrevista;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Editor extends Usuario {
     private String journal;
     private String usuario;
@@ -50,4 +59,78 @@ public class Editor extends Usuario {
         }
     }*/
 
+    /**
+     * Método para asignar un artículo a revisores.
+     *
+     * @param articulo El artículo a asignar
+     * @param revisores Lista de revisores a asignar
+     * @param listaGestion Lista donde se guardará el proceso de gestión del artículo
+     */
+
+
+    
+
+
+    public void asignarArticulo(Articulo articulo, ArrayList<Revisor> revisores, ArrayList<GestionarArticulo> listaGestion) {
+        GestionarArticulo gestionArticulo = new GestionarArticulo(articulo, revisores, this, "Fecha de asignación");
+        listaGestion.add(gestionArticulo);
+        System.out.println("Artículo asignado correctamente a revisores.");
+    }
+
+    /**
+     * Método para enviar un artículo a publicación una vez revisado y aceptado.
+     *
+     * @param articulo El artículo a enviar a publicación
+     */
+    public void enviarArticuloAPublicacion(Articulo articulo) {
+        if (articulo.getEstadoArticulo() == EstadoAriculo.ACEPTADO) {
+            // Lógica para enviar el artículo a la sección de publicación
+            System.out.println("Artículo enviado a publicación con éxito.");
+        } else {
+            System.out.println("El artículo no puede ser enviado a publicación porque no ha sido aceptado.");
+        }
+    }
+
+    /**
+     * Método para rechazar un artículo que no cumple con los criterios de la revista.
+     *
+     * @param articulo El artículo a rechazar
+     */
+    public void rechazarArticulo(Articulo articulo) {
+        if (articulo.getEstadoArticulo() == EstadoAriculo.REVISION) {
+            articulo.setEstadoAriculo(EstadoAriculo.RECHAZADO);
+            System.out.println("Artículo rechazado.");
+        } else {
+            System.out.println("El artículo no puede ser rechazado en este momento.");
+        }
+    }
+
+    /**
+     * Método para notificar a los autores sobre el estado de sus artículos.
+     *
+     * @param listaGestion Lista de artículos gestionados
+     */
+    public void notificarAutores(ArrayList<GestionarArticulo> listaGestion) {
+        for (GestionarArticulo gestion : listaGestion) {
+            System.out.println("Notificando al autor: " + gestion.getArticulo().getDatosAutor().getCorreo());
+            // Lógica para enviar notificación por correo u otro medio
+        }
+        System.out.println("Notificaciones enviadas a todos los autores.");
+    }
+
+    // Método toString para representar el objeto como cadena
+    @Override
+    public String toString() {
+        return "Editor{" +
+                "nombre='" + getNombre() + '\'' +
+                ", apellido='" + getApellido() + '\'' +
+                ", correoElectronico='" + getCorreo() + '\'' +
+                ", journal='" + journal + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", contraseña='" + contraseña + '\'' +
+                '}';
+    }
 }
+
+
+
